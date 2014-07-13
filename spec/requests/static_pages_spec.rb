1bf2871142b4b279960f43150018eb1ad357c0a9
Aos <aos.diva.lcbdbsde@gmail.com>
2014-07-14 00:50:10 +0900

    Message

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
   describe "Home pages" do
     
     it "should have the content 'Sample App'" do
@@ -9,11 +10,17 @@ describe "StaticPages" do
     end
   
   
-  it "should have the title 'Home'" do
+  it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+    
+     it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
+  
 
 describe "Help page" do
 
@@ -25,7 +32,7 @@ describe "Help page" do
   
 it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title}")
     end
   end
 
@@ -39,7 +46,20 @@ it "should have the title 'Help'" do
     
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+    end
+   
+  describe "Contact" do
+    
+    it "should have the content 'Contact'" do 
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
     
   end
